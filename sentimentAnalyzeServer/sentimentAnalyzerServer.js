ab56f79365a212c297c86e747756a2690fd36488
Sajid Zaman <zsajid014@gmail.com>
2021-02-19 16:22:41 +0000

    Changed express file

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -6,6 +6,42 @@ app.use(express.static('client'))
 const cors_app = require('cors');
 app.use(cors_app());
 
+const dotenv = require("dotenv");
+dotenv.config();
+
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+    version: '2020-08-01',
+    authenticator: new IamAuthenticator({
+        apikey: '{api_key}',
+    }),
+    serviceUrl: '{api_url}',
+    });
+
+    const analyzeParams = {
+    'url': 'www.ibm.com',
+    'features': {
+        'concepts': {
+        'limit': 3
+        }
+      }
+    };
+
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+    .then(analysisResults => {
+        console.log(JSON.stringify(analysisResults, null, 2));
+    })
+    .catch(err => {
+        console.log('error:', err);
+    });
+}
+
 app.get("/",(req,res)=>{
     res.render('index.html');
   });
